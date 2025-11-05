21392469af30e5c5f99dd8e7dc64044f3d84f6ff
Thomas Mwanzia <thomasmwanziamatheri@gmail.com>
2025-11-06 02:27:39 +0300

    Fixed backend API serving and ToastProvider import

--- websitesarena/frontend-next/src/app/layout.js
+++ websitesarena/frontend-next/src/app/layout.js
@@ -1,8 +1,9 @@
 import '@fontsource/cinzel-decorative/400.css';
 import '@fontsource/cinzel-decorative/700.css';
 import './globals.css';
-/* import ToastProvider from '@/components/ToastProvider';
- */
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import { Toaster } from 'react-hot-toast';
 export const metadata = {
   title: 'Websites Arena',
   description: 'Web and Mobile App Development Agency',
@@ -11,10 +12,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children}
-{/*         <ToastProvider />
- */}      </body>
+      <body className="bg-gray-900 text-white">
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+        <Toaster
+          position="center-top"
+          reverseOrder={false}
+        />
+      </body>
     </html>
   );
 }
